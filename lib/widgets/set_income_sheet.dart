15ae655ca1de4c95d200f1f10298d96c9b0ee429
Sabiha Niaz <sabihaniaz7@gmail.com>
2026-03-30 10:44:20 +0500

    feat: add new features and update related components

---
+++ lib/widgets/set_income_sheet.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '../theme/app_theme.dart';
+import 'package:provider/provider.dart';
+import '../providers/expense_provider.dart';
+
+class SetIncomeSheet extends StatefulWidget {
+  const SetIncomeSheet({super.key});
+
+  @override
+  State<SetIncomeSheet> createState() => _SetIncomeSheetState();
+}
+
+class _SetIncomeSheetState extends State<SetIncomeSheet> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
